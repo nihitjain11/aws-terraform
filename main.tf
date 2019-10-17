578f447ab4489f8ad012d9af9005cf522a7f3759
Nihit Jain <nihit.jain@tothenew.com>
2019-10-17 14:25:16 +0530

    vpc v5

--- main.tf
+++ main.tf
@@ -8,14 +8,14 @@ resource "aws_vpc" "vpc" {
 
 resource "aws_subnet" "private" {
   vpc_id                          = "${aws_vpc.vpc.id}"
-  cidr_block                      = ["10.0.1.0/24", "10.0.2.0/24"]
-  availability_zone               = ["us-east-1a", "us-east-1b"]
+  cidr_block                      = "10.0.1.0/24"
+  availability_zone               = "us-east-1a"
 }
 
 resource "aws_subnet" "public" {
   vpc_id                          = "${aws_vpc.vpc.id}"
-  cidr_block                      = ["10.0.3.0/24", "10.0.4.0/24"]
-  availability_zone               = ["us-east-1a", "us-east-1b"]
+  cidr_block                      = "10.0.3.0/24"
+  availability_zone               = "us-east-1a"
   
 }
 
@@ -27,11 +27,11 @@ resource "aws_route_table" "rt" {
   vpc_id = "${aws_vpc.vpc.id}"
 
   route {
-    cidr_block = ["10.0.1.0/24","10.0.2.0/24"]
+    cidr_block = "10.0.1.0/24"
   }
 
   route {
-    cidr_block = ["10.0.3.0/24","10.0.4.0/24"]
+    cidr_block = "10.0.3.0/24"
     gateway_id = "${aws_internet_gateway.gw.id}"
   }    
 }
