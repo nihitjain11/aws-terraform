107d2776a12195061285f3aeb446b740cb852ace
Nihit Jain <nihit.jain@tothenew.com>
2019-10-17 14:20:52 +0530

    vpc v4

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ provider "aws" {
 }
 resource "aws_vpc" "vpc" {
     cidr_block      = "10.0.0.0/16"
-    
-    enable_nat_gateway = true
-    single_nat_gateway = true
-    one_nat_gateway_per_az = false
 }
 
 resource "aws_subnet" "private" {
