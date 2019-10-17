2db4be4d7002429918b718dc22867500c30c90c8
Nihit Jain <nihit.jain@tothenew.com>
2019-10-17 13:35:12 +0530

    vpc 2

--- modules/vpc/modules/subnets/main.tf
+++ modules/vpc/modules/subnets/main.tf
@@ -1,12 +1,12 @@
 resource "aws_subnet" "public" {
-  vpc_id                          = .vpc_id
+  vpc_id                          = aws_vpc.this.vpc_id
   cidr_block                      = element(concat(var.public_subnets, [""]), count.index)
   availability_zone               = element(var.azs, count.index)
   map_public_ip_on_launch         = var.map_public_ip_on_launch
 }
 
 resource "aws_subnet" "private" {
-  vpc_id                          = local.vpc_id
+  vpc_id                          = aws_vpc.this.vpc_id
   cidr_block                      = var.private_subnets[count.index]
   availability_zone               = element(var.azs, count.index)
 }
